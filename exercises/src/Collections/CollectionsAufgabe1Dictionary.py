95c62c8f0a9f21de5a362b2fc4dabfce7687b18c
Unknown <ansc0098@stud.hs-kl.de>
2018-12-18 11:56:26 +0100

    -bei den Lösungen  Ausgabe der Printbefehle einfügen -newpage entfernt -Aufgabe1 zu List bearbeitet

--- exercises/src/Collections/CollectionsAufgabe1Dictionary.py
+++ exercises/src/Collections/CollectionsAufgabe1Dictionary.py
@@ -1,5 +1,6 @@
 dictionary = dict(A="Alpha", L="Lima", E="Echo", X="Xray")
 print(dictionary)
-
+# Ausgabe: {'A': 'Alpha', 'L': 'Lima', 'E': 'Echo', 'X': 'Xray'}
 
 print(dictionary.values())
+# Augabe: dict_values(['Alpha', 'Lima', 'Echo', 'Xray'])
