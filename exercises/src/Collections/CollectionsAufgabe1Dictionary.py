5ee6eeaa3b3e138fb004e95cbf89cfe90e4a654d
Unknown <ansc0098@stud.hs-kl.de>
2018-12-19 18:12:38 +0100

    Textrand Anpassung

--- exercises/src/Collections/CollectionsAufgabe1Dictionary.py
+++ exercises/src/Collections/CollectionsAufgabe1Dictionary.py
@@ -1,11 +1,13 @@
 dictionary = dict(A="Alpha", L="Lima", E="Echo", X="Xray")
 print(dictionary)
-# Ausgabe: {'A': 'Alpha', 'L': 'Lima', 'E': 'Echo', 'X': 'Xray'}
+# Ausgabe: {'A': 'Alpha', 'L': 'Lima', 'E': 'Echo',
+#           'X': 'Xray'}
 
 print(dictionary.values())
 # Augabe: dict_values(['Alpha', 'Lima', 'Echo', 'Xray'])
 
-dictionary1 = dict(N="November", A="Alpha", T="Tango", O="Oscar" )
+dictionary1 = dict(N="November", A="Alpha",
+                   T="Tango", O="Oscar" )
 dictionary2 = {}
 
 for d in (dictionary, dictionary1):
