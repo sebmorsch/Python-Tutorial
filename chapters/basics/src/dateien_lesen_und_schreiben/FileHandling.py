8337fdbf0074ba9cd1a91eea42035fb69aeb813c
Unknown <jube0010@stud.hs-kl.de>
2019-01-05 21:44:01 +0100

    Kapitel: Dateien lesen und schreiben
    - Kapitel Dateien lesen und schreiben hinzugefügt
    - Code hinzugefügt
    - grundlagen.tex vorläufig abgeändert

---
+++ chapters/basics/src/dateien_lesen_und_schreiben/FileHandling.py
@@ -0,0 +1,10 @@
+# src/FileHandling.py
+# Typ des Datei-Objekts festlegen
+# Beispiel am "r"-Modus
+
+file = open("datei.txt", "rt")  # Text
+# oder
+file = open("datei.txt", "r")  # Text
+
+file = open("datei.txt", "rb")  # Binaer
+flie.close()
