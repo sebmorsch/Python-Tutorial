33391ce91c28661d391c49007458b14fbf71a7f0
Christoph Seibel <chriseibel@gmx.de>
2019-01-17 10:38:25 +0100

    Zeilenumbruch in Code eingefügt -> Ging über Rand hinaus

--- chapters/userInterface/src/GUI_task02.py
+++ chapters/userInterface/src/GUI_task02.py
@@ -1,11 +1,12 @@
 # gui/GUI_task02.py
-# Lösung der Aufgabe 2 Kapitel Benutzeroberflächen
+# Loesung der Aufgabe 2 Kapitel Benutzeroberflaechen
 
 from tkinter import *
 
 root = Tk()
 
-label = Label(root, text="Hello Python-World!").grid(row=0, column=0)
+label = Label(root, text="Hello Python-World!").grid(row=0,
+column=0)
 label2 = Label(root, text="Label 2").grid(row=0, column=1)
 label3 = Label(root, text="Label 3").grid(row=1, column=0)
 label4 = Label(root, text="Label 4").grid(row=1, column=1)
