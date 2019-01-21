e0357d174a92e6f4ed4c68672165c26695ff2b23
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2019-01-21 16:35:21 +0100

    Startet synchronisation subsection. Added Synchronisation_error example.

---
+++ exercises/src/nebenlaufigkeit/beispiel_synchronisation_fehler.py
@@ -0,0 +1,29 @@
+import threading
+
+class Counter:
+    def __init__(self):
+        self.count = 0
+    
+    def increment(self):
+        self.count += 1
+
+class IncrementerThread(threading.Thread):
+    def __init__(self, counter):
+        threading.Thread.__init__(self)
+        self.counter = counter
+    
+    def run(self):
+        for i in range(1000000):
+            self.counter.increment()
+
+counter = Counter()
+threads = []
+for i in range(8):
+    thread = IncrementerThread(counter)
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()
+
+print(counter.count)
