f3461984b0d7af840b5857bce6940ca2dcd055bd
Matthias Haselmaier <matthias.haselmaier@hs-kl.de>
2019-01-21 20:39:12 +0100

    Refactored content to conform to our guidelines. Added Solution and Source for exercises.

--- exercises/src/nebenlaufigkeit03.py
+++ exercises/src/nebenlaufigkeit03.py
@@ -1,20 +1,25 @@
 import threading
 
+
 class Counter:
     def __init__(self):
         self.count = 0
-    
+        self.lock = threading.Lock()
+
     def increment(self):
-        self.count += 1
+        with self.lock:
+            self.count += 1
+
 
 class IncrementerThread(threading.Thread):
     def __init__(self, counter):
         threading.Thread.__init__(self)
         self.counter = counter
-    
+
     def run(self):
         for i in range(1000000):
             self.counter.increment()
+
 
 counter = Counter()
 threads = []
